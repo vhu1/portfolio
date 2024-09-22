0720e6671bb53aea52f0a643f167250c7dfb96c6
vhu1 <bhuwan7b@gmail.com>
2024-09-23 03:17:21 +1000

    first

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ window.addEventListener("load", () => {
     const loadingScreen = document.getElementById("loading-screen");
     setTimeout(() => {
         loadingScreen.classList.add("hidden");
-    }, 2500); // Keep the loading screen for 2.5 seconds
+    }, 1500); // Keep the loading screen for 2.5 seconds
 });
 
 // Smooth scrolling functionality
